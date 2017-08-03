0dac4d323f6409afe1ab5f4091e840431ab92255
huxy11 <huxy11@yeah.net>
2017-08-03 16:38:37 +0800

    Initial Commit

---
+++ Leetcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leetcode
+//
+//  Created by 胡星宇 on 2017/8/3.
+//  Copyright © 2017年 胡星宇. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
